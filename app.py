95b60f748b5393d31745078a1a6d65a544c98b45
zaldiabdulhadi <zaldiabdulhadi12@gmail.com>
2025-07-17 12:21:18 +0700

    add requirements

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title('Bot Chat Sederhana dengan Gemini')
 
 # Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = []  
     
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
